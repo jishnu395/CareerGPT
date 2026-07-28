fb750f3153915eaf57ef09b85d93ad054e28fd3f
jishnu <vjishnu744@gmail.com>
2026-07-28 11:33:14 +0530

    Use Render backend URL

--- careergpt-frontend/src/services/api.js
+++ careergpt-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://careergpt-backend-df4t.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
